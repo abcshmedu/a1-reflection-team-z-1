2c6cbc6e4d35bdc96d96a87747199e971846102f
Markus Kral <kral0@hm.edu>
2017-03-27 16:04:52 +0200

    did first stuff

--- src/main/java/RenderMe.java
+++ src/main/java/RenderMe.java
@@ -1,5 +1,22 @@
 /**
  * Created by Maximilian on 27.03.2017.
- */
+
+**/
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE) //on class level
+
 public @interface RenderMe {
+
+    int number() default 0;
+    String[] stringArray() default "";
+
+    String createdBy() default "";
+
+    String lastModified() default "01/01/19901";
 }
